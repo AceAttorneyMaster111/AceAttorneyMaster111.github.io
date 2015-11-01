2981ac2f4b664b0d3502f82d6612ca7c4eb46361
AceAttorneyMaster111 <NoahS2003@gmail.com>
2015-11-01 16:59:39 -0500

    Initialized index page and common page, php&css

---
+++ common/common.php
@@ -0,0 +1,20 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="common.css">
+</head>
+<?php
+	function commonheader()
+	{
+		?>
+		<div class="header"
+			<nav>
+				<a href="index.php"><button type="button">Home</button></a>
+				<!--First Case (unnamed)-->
+				<button type="button"><span class="comingsoon">Coming soon!</span></button>
+				<!--Second Case (unnamed)-->
+				<button type="button"><span class="comingsoon">Coming soon!</span></button>
+				<a href="help.php"><button type="button">Help</button></a>
+			</nav>
+		</div>
+		<?php
+	}
+?>
